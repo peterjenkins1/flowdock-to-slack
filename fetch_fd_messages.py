18bc70c7341b09f3221fa76385dced024533b136
Peter Jenkins <peter.jenkins@smartly.io>
2020-06-10 13:18:49 +0300

    Make fetch_fd_messages more readable

--- fetch_fd_messages.py
+++ fetch_fd_messages.py
@@ -33,13 +33,14 @@ browser['user_session[email]'] = fd_username
 browser['user_session[password]'] = fd_password
 
 login_response = browser.submit_selected()
+request_headers = login_response.request.headers
 
 flow_name_regex = r"/([a-zA-Z-]+)-2020-"
 
 for fd_export in fd_exports:
 	print(fd_export)
 	flow_name = re.findall(flow_name_regex, fd_export)[0]
-	zip = RemoteZip(fd_export, headers=login_response.request.headers)
+	zip = RemoteZip(fd_export, headers=request_headers)
 	messages_file = zip.getinfo('messages.json')
 	output_dir = '%s/%s/' % (download_dir, flow_name)
 	zip.extract(messages_file, output_dir)
